af7eaa66d7144034577aa32fccba6eca530d70da
miachang <mchang@linkernetworks.com>
2017-10-31 09:22:00 +0800

    fix index

--- python/model.py
+++ python/model.py
@@ -5,8 +5,8 @@ def azureml_main(dataframe1 = None, dataframe2 = None):
     from sklearn.ensemble import RandomForestClassifier        
        
     clf = RandomForestClassifier(n_estimators=25)
-    X_train = np.array(dataframe1.ix[:,2:])
-    y_train = np.array(dataframe1.ix[:,1])
+    X_train = np.array(dataframe1.ix[:,1:])
+    y_train = np.array(dataframe1.ix[:,0])
     clf.fit(X_train, y_train)    
     state = pickle.dumps(clf)
     ret = pd.DataFrame(data = [state], columns=["Serialized Model"])    
